a7655547017f1c5812514880727c40b67b4eced7
cgx372002622 <372002622@qq.com>
2024-03-17 16:27:56 +0800

    fix

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,6 @@
 import { MongoClient, ObjectId } from "mongodb";
 
-// 等待其他组员提供MongoDB连接字符串
+// TODO 等待其他组员提供MongoDB连接字符串
 process.env.MONGODB_URI =
   "mongodb://<username>:<password>@<endpoint>.documents.azure.com:10255/?ssl=true";
 
@@ -12,7 +12,7 @@ if (!process.env.MONGODB_URI) {
 // Connect to MongoDB
 async function connectToDB() {
   const client = await MongoClient.connect(process.env.MONGODB_URI);
-  // 替换成对应数据库
+  // TODO 替换成对应数据库
   const db = client.db("bookingsDB");
   db.client = client;
   return db;
